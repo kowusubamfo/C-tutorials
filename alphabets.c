6306e5e6a7c67ef5df8a8f7ddb25a5480aa9d284
kowusubamfo <kowusubamfo@gmail.com>
2023-03-09 10:12:53 +0000

    Initial commit

---
+++ alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("You the man OBJ🔥\n");
+    printf("\n");
+    printf(" _____     _____   _____\n");
+    printf("|     |   |     | |     |\n");
+    printf("|     |   |_____|       |\n");
+    printf("|     |   |     |       |\n");
+    printf("|_____|   |_____|  _____|\n");
+    return 0;
+}
