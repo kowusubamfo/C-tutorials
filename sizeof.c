f907bee224946099191f69166115a75bd5ee7130
kowusubamfo <kowusubamfo@gmail.com>
2023-03-09 15:22:56 +0000

    Updated sizeof

--- sizeof.c
+++ sizeof.c
@@ -2,6 +2,9 @@
 
 int main()
 {
-    printf("%zu byte(s)\n", sizeof(char));
+    printf("Size of \"int\" is %lu byte(s)\n", sizeof(char));
+    printf("Size of \"char\" is %lu byte(s)\n", sizeof(char));
+    printf("Size of \"float\" is %lu byte(s)\n", sizeof(float));
+    printf("Size of \"double\" is %lu byte(s)\n", sizeof(double));
     return 0;
 }
