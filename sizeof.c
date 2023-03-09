b9803fff45ac6cbd18b3f31f8a1a18863259eead
kowusubamfo <kowusubamfo@gmail.com>
2023-03-09 15:31:44 +0000

    Updated sizeof

--- sizeof.c
+++ sizeof.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * Description: A function that uses sizeof operator
+ * to print sizes of various data types
+ * return: 0
+*/
+
 int main()
 {
-    printf("Size of \"int\" is %lu byte(s)\n", sizeof(char));
-    printf("Size of \"char\" is %lu byte(s)\n", sizeof(char));
-    printf("Size of \"float\" is %lu byte(s)\n", sizeof(float));
-    printf("Size of \"double\" is %lu byte(s)\n", sizeof(double));
+
+    int a;
+    char b;
+    float c;
+    double d;
+
+    printf("Size of \"int\" is %lu byte(s)\n", sizeof(a));
+    printf("Size of \"char\" is %lu byte(s)\n", sizeof(b));
+    printf("Size of \"float\" is %lu byte(s)\n", sizeof(c));
+    printf("Size of \"double\" is %lu byte(s)\n", sizeof(d));
     return 0;
 }
