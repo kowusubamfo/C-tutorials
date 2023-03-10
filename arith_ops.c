cb138227dd71b10f139c6aef2869d4a7764715a6
kowusubamfo <kowusubamfo@gmail.com>
2023-03-10 14:43:03 +0000

    Added arith_ops file

---
+++ arith_ops.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+        int a = 2, b = 3, c = 4, d =5, e;
+
+        e = ++a;
+        printf("The answer for the operation ++a operation is %d.\n", e);
+
+        return 0;
+}
